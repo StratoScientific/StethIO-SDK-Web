36dc5d21683f9c61f913a0cabb15d6b18c545fdb
Harini <harini.p@ionixxtech.com>
2024-04-08 12:41:46 +0530

    latest changes

--- sdk-test-app-angular/src/app/app.component.ts
+++ sdk-test-app-angular/src/app/app.component.ts
@@ -13,13 +13,30 @@ export class AppComponent implements OnInit {
   playbackSpeedList = [0.25, 0.5, 0.75, 1, 1.25, 1.50, 1.75, 2];
   speed = 1;
   audioEngine;
+  audioContext;
   mode;
   volume = 1;
+  audioEngine1: any;
 
   constructor() {
   }
 
   ngOnInit() {
+    this.initAudio().then(r => console.log("Initialted"));
+  }
+
+
+  async initAudio() {
+    this.audioEngine = await new AudioEngine.default().initAudioEngine('canvas', 'va1vuCRG3fGDGB2ddGwjqQ==');
+    let constraints = { audio: true } // add video constraints if required
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(async (stream) => {
+        this.audioContext = new AudioContext();
+        this.audioEngine.setAutoGainFlag(true);
+        this.audioEngine.setFilterFlag(true);
+        await this.audioEngine.startIt(false);
+        this.audioEngine.loadOpusDecoder();
+      })
   }
 
   modeSelected(event) {
@@ -30,31 +47,35 @@ export class AppComponent implements OnInit {
   speedSelected(event) {
     this.speed = event.target.value;
     if(this.audioEngine) {
+      // this.audioEngine.pause();
       this.audioEngine.playbackSpeedChange(this.speed);
+      // this.audioEngine.resume();/
     }
   }
 
   volumeSet(event) {
     if(this.audioEngine) {
       this.volume = event.target.value;
-      this.audioEngine.volumeSet(this.volume);
+      let volume = this.volume/100;
+      this.audioEngine.volumeSet(volume);
     }
   }
-  
+
   volumeUp(up) {
     this.volume = up ? (Number(this.volume) + 1) : (Number(this.volume) - 1);
     if(this.audioEngine) {
-      this.audioEngine.volumeSet(this.volume);
+      let volume = this.volume/100;
+      this.audioEngine.volumeSet(volume);
     }
   }
 
   async heartData() {
-    this.audioEngine = await AudioEngine.initAudioEngine('canvas','va1vuCRG3fGDGB2ddGwjqQ==');
-    console.log(this.audioEngine);
+    this.audioEngine = await new AudioEngine.default().initAudioEngine('canvas','va1vuCRG3fGDGB2ddGwjqQ==');
+    this.audioEngine1 = await new AudioEngine.default().initAudioEngine('canvas1','va1vuCRG3fGDGB2ddGwjqQ==');
   }
 
   async lungData() {
-    this.audioEngine = await AudioEngine.initAudioEngine('canvas','va1vuCRG3fGDGB2ddGwjqQ==', {mode: 'LUNG'});
+    this.audioEngine = await AudioEngine.default.initAudioEngine('canvas','va1vuCRG3fGDGB2ddGwjqQ==', {mode: 'LUNG'});
   }
 
   start() {
@@ -65,8 +86,20 @@ export class AppComponent implements OnInit {
     }
   }
 
+  start1() {
+    if(this.audioEngine1) {
+      this.audioEngine1.startIt(false).then((res) => {
+        this.audioEngine1.loadOpusDecoder();
+      });
+    }
+  }
+
   pause() {
     this.audioEngine.pause();
+  }
+
+  pause1() {
+    this.audioEngine1.pause();
   }
 
   resume() {
@@ -81,24 +114,36 @@ export class AppComponent implements OnInit {
     this.mode == 'HEART' ? this.createHeartInputFiles() : this.createLungInputFiles();
   }
 
+  loadAudioFile1() {
+    this.mode == 'HEART' ? this.createHeartInputFiles1() : this.createLungInputFiles();
+  }
+
   // load audio from local file
   createLungInputFiles() {
-    const audioContext = new AudioContext();
     fetch('assets/lung.wav')
     .then(response => response.arrayBuffer())
-    .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+    .then(arrayBuffer => this.audioContext.decodeAudioData(arrayBuffer))
+    .then(audioBuffer => {
+        this.audioEngine.testAudioInput(audioBuffer.getChannelData(0));
+    });
+
+  }
+
+  createHeartInputFiles() {
+    fetch('assets/heart.wav')
+    .then(response => response.arrayBuffer())
+    .then(arrayBuffer => this.audioContext.decodeAudioData(arrayBuffer))
     .then(audioBuffer => {
         this.audioEngine.testAudioInput(audioBuffer.getChannelData(0));
     });
   }
 
-  createHeartInputFiles() {
-    const audioContext = new AudioContext();
+  createHeartInputFiles1() {
     fetch('assets/heart.wav')
     .then(response => response.arrayBuffer())
-    .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+    .then(arrayBuffer => this.audioContext.decodeAudioData(arrayBuffer))
     .then(audioBuffer => {
-        this.audioEngine.testAudioInput(audioBuffer.getChannelData(0));
+        this.audioEngine1.testAudioInput(audioBuffer.getChannelData(0));
     });
   }
 
@@ -107,13 +152,12 @@ export class AppComponent implements OnInit {
     navigator.mediaDevices.getUserMedia({audio: true})
     .then((stream) => {
       let self = this;
-      const context = new AudioContext({sampleRate: 16000});
+      const context = new AudioContext({sampleRate: 44100});
         const source = context.createMediaStreamSource(stream);
         const processor = context.createScriptProcessor(1024, 1, 1);
-    
-        source.connect(processor);
-        processor.connect(context.destination);
-    
+
+        // source.connect(processor);
+        // processor.connect(context.destination);
         processor.onaudioprocess = function(e) {
           self.playAudioInput(e.inputBuffer);
         };
@@ -124,4 +168,27 @@ export class AppComponent implements OnInit {
     this.audioEngine.testAudioInput(audioBuffer.getChannelData(0));
   }
 
+   muteMe(elem:any) {
+     console.log(elem)
+    elem.muted = true;
+  }
+
+  mute() {
+    console.log(document.querySelectorAll("video, audio"))
+    document.querySelectorAll("video, audio").forEach((elem) => this.muteMe(elem));
+  }
+
+  unMute() {
+    this.volume = 1;
+    this.audioEngine.volumeSet(1);
+  }
+
+  async startAll() {
+    fetch('assets/heart.wav')
+    .then(response => response.arrayBuffer())
+    .then(arrayBuffer => this.audioContext.decodeAudioData(arrayBuffer))
+    .then(audioBuffer => {
+      this.audioEngine.testAudioInput(audioBuffer.getChannelData(0));
+    });
+  }
 }
